df2392a1f0a2adee04620f8c3e2cf778046fb02a
RaweNist <116841332+RaweNist@users.noreply.github.com>
2026-05-26 23:44:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,3 +5,17 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+std::vector<std::vector<char>> getAllPerms(PMTree tree) {
+  std::vector<std::vector<char>> result;
+  tree.get(result);
+  return result;
+}
+
+std::vector<char> getPerm1(PMTree& tree, int num) {
+  std::vector<std::vector<char>> all = getAllPerms(tree);
+  return all[num-1];
+}
+
+std::vector<char> getPerm2(PMTree& tree, int num) {
+  return tree.getPermByNumber(num-1);
+}
